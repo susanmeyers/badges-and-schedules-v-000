e33497b76e3265aa4f29a44d343bd2287452e481
Susan Meyers <smmeyers24@gmail.com>
2018-08-13 16:00:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+#write a badge_maker method with an argument of (name)
+#badge should read "Hello, my name is __."
+#speakers are:Edsger, Ada, Charles, Alan, Grace, Linus, and Matz.
+require 'pry'
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+
+def assign_rooms(room)
+  room.each_with_index do
+  room.collect do |room|
+  batch_badge_creator(name)
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+  end
+end
+   #{}"Hello, #{name}! You'll be assigned to room #{index}!"
