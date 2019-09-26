c65c7bbb052999d0af0a4f7ed60067c042ddb1c8
Susan Meyers <smmeyers24@gmail.com>
2019-09-26 16:46:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(names)
+  
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+  names.collect do |name|
+    puts "Hello my name is #{name}."
+    
+  end
+  
+end
+
+
+
+def assign_rooms(speaker, room)
+  
+  puts "Hello, #{speaker! You'll be assigned to room #{room}!"
+  
+end
