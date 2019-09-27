1528dbd6c74010c045a052f07f566e625307b820
Susan Meyers <smmeyers24@gmail.com>
2019-09-27 02:59:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
@@ -11,5 +12,16 @@ def assign_rooms(names)
   names.collect.with_index do |name, index|
     #binding.pry
   "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts (attendee)
+    
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts (attendee)
+    print attendee
   end
 end
